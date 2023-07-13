2dc4493bf3aec056bb759bd180b377b2edd3069e
nadaibrahim-png <inada350@gmail.com>
2023-07-13 10:23:48 -0700

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *My hello, printf task
+ *Return: return 1 after ptogram execution
+ */
+int main(void)
+{
+printf("with proper grammer, but the outcome is a pieceof art,\n");
+return(0);
+}
